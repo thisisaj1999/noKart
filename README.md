736984f93ab650aa1d2db13fb2808832226a7838
thisisaj1999 <atuljoshi94170@gmail.com>
2024-03-17 21:00:01 +0400

    [Init]: Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TS
+
+### An incomplete project
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './App.module.scss'
+const App: React.FC = () => {
+  return (
+    <div className={style.style}>App</div>
+  )
+}
+
+export default App
